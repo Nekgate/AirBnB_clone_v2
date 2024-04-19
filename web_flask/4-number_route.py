e32e1e4439dd4118e025a0ef02844a4ee5fb3455
Nekgate <Nekgate@gmail.com>
2024-04-19 19:27:03 +0100

    A script that starts a Flask web application.

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,9 +5,11 @@ Routes:
     /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
     /c/<text>: Displays 'C' followed by the value of <text>.
-     /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
 """
 from flask import Flask
+from flask import abort
 
 app = Flask(__name__)
 
@@ -41,6 +43,12 @@ def python(text="is cool"):
     """
     text = text.replace("_", " ")
     return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Display 'n is a number' only if n is an integer."""
+    return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
